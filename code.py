9cd90da4f27a6c69394ceab7f0fac83cb82f09fd
Shan Zhong <zhongshanyes@gmail.com>
2019-12-05 02:17:57 -0500

    Shan

---
+++ code.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  5 02:04:02 2019
+
+@author: zhong
+"""
+
+f = open("C:\\Users\\zhong\\Downloads\\fanrenxiuxianchuan_wangyu.txt" , encoding="gbk")
+print(f.read())
